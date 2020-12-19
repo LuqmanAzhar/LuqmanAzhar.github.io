138b689e32a58b9e36e28726a62703f0931b3a88
Luqman Azhar <Luqman_Azhar@hotmail.com>
2020-12-20 01:56:31 +0800

    Added a dynamic nav bar

---
+++ js/navbtn.js
@@ -0,0 +1,16 @@
+function openPage(pageName, element) {
+  var i, btncontent, navlinks;
+  btncontent = document.getElementsByClassName("btn-content");
+  for (i = 0; i < btncontent.length; i++) {
+    btncontent[i].style.display = "none";
+  }
+  navlinks = document.getElementsByClassName("nav-btn");
+  for (i = 0; i < navlinks.length; i++) {
+    navlinks[i].className = navlinks[0].className.replace(" active", "");
+  }
+  document.getElementById(pageName).style.display = "block";
+  console.log(element)
+  element.className += " active"
+}
+
+document.getElementById("defaultopen").click();
